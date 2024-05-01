5275cf8e3c54fb4c5cb4ac871dcd4ec27637dc72
lucaspari <lucas.pari@icloud.com>
2024-04-30 22:05:29 -0300

    adding new security group

--- main.tf
+++ main.tf
@@ -2,10 +2,37 @@ provider "aws" {
   profile = "default"
   region  = "sa-east-1"
 }
+resource "aws_security_group" "instance_sg" {
+  name        = "instance-sg"
+  description = "Security group for EC2 instances"
+  vpc_id      = var.defaultvpc
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow traffic from anywhere
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"] # Allow all outbound traffic
+  }
+}
+
+resource "aws_lb" "test" {
+  name               = "test-lb-tf"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.instance_sg.id]
+  subnets            = [for net in var.subnet : net]
+}
 resource "aws_instance" "server" {
   ami                    = var.linux_image
   instance_type          = var.istance_type
-  vpc_security_group_ids = [var.security_groups]
+  vpc_security_group_ids = [aws_security_group.instance_sg.id]
   user_data              = <<-EOF
                                     #!/bin/bash
                                     # Use this for your user data (script from top to bottom)
@@ -34,13 +61,6 @@ resource "aws_instance" "server2" {
 EOF
 }
 
-resource "aws_lb" "test" {
-  name               = "test-lb-tf"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [var.security_groups]
-  subnets            = [for net in var.subnet : net]
-}
 
 resource "aws_lb_target_group" "targetgptest" {
   name     = "tf-example-lb-tg"
